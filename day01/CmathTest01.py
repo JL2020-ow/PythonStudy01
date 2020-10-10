58539006a47233f0c088b45050efb963d5f4a344
JL2020-ow <williamalex22@126.com>
2020-10-10 15:41:03 +0800

    first commit

---
+++ day01/CmathTest01.py
@@ -0,0 +1,4 @@
+import cmath
+x=-1
+b=cmath.sqrt(-1)
+print(b)
---
+++ day02/IndexTest01.py
@@ -0,0 +1,7 @@
+greeting = 'Hello'
+print(greeting[0])
+print(greeting[-1])
+print(greeting[1])
+forth = input('Year:')[3]
+print(forth)
+
